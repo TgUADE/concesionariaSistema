1f60cd1d23602fe4a969769c59f8b55da0e28eef
Tomas <tguzzo@uade.edu.ar>
2025-05-21 08:53:48 -0300

    firts commit

---
+++ src/main/java/com/grupo9/sistemaConcesionaria/SistemaConcesionariaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.sistemaConcesionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaConcesionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaConcesionariaApplication.class, args);
+	}
+
+}
